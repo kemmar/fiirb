203fe1b087c77ff9b8fd62340a26b9417c8541c1
brianlewis <lewis_737@hotmail.com>
2016-06-11 01:00:37 +0100

    [Brian] hello world endpoint

--- src/main/scala/application/Application.scala
+++ src/main/scala/application/Application.scala
@@ -4,5 +4,5 @@ import com.twitter.finagle.Http
 import com.twitter.util.Await
 
 object Application extends App {
-  Await.ready(Http.server.serve(":8081", RouteConfig.buildRoutes))
+  Await.ready(Http.server.serve(s":${System.getenv("PORT")}", RouteConfig.buildRoutes))
 }
